4c0f79fe34e0a4643bc468bd10e1adcd3d25c6b8
kapil-awadhwal <56359754+kapil-awadhwal@users.noreply.github.com>
2019-11-04 00:11:53 +0530

    Added functionality for deletion of items from the Cart

--- src/main/java/com/pluralsight/CartController.java
+++ src/main/java/com/pluralsight/CartController.java
@@ -45,6 +45,9 @@ public class CartController extends HttpServlet {
 				case "/addcart":
 					 addToCart(request, response);
            break;
+				case "/delete":
+						deleteFromCart(request,response);
+						break;
         default:
            break;
 			}
@@ -56,6 +59,14 @@ public class CartController extends HttpServlet {
 		response.sendRedirect("../ShoppingCart.jsp");
 	}
 
+	private void deleteFromCart() (HttpServletRequest request, HttpServletResponse response)
+		throws ServletException, IOException {
+			HttpSession session = request.getSession();
+			int index = Integer.parseInt(request.getParameter("index"));
+			ShoppingCart shoppingCart = (ShoppingCart)session.getAttribute("cart");
+			shoppingCart.deleteCartItem(index);
+
+		}
   protected void addToCart(HttpServletRequest request, HttpServletResponse response)
 		throws ServletException, IOException {
    HttpSession session = request.getSession();
